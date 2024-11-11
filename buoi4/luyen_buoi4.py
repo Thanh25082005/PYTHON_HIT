4816a178ff478a2fbc8c9854068763db20f5d8a7
Thanh25082005 <thanh2005.hs@gmail.com>
2024-11-11 12:34:05 +0700

    first commit

---
+++ buoi4/luyen_buoi4.py
@@ -0,0 +1,52 @@
+# l = list(map(int, input().split()))
+# print(l)
+
+# sum = 0
+# for i in range(len(l)):
+#     sum += l[i]
+# print(sum)
+# n = int(input())
+# c = [[int(input()) for i in range(n) ] for j in range(n)]
+
+# l = list(map( int , input().split() ))
+
+# matrix = [ list(map( int , input().split() )) for i in range(5)]
+# print(matrix)
+
+n = int(input())
+ar  = list(map(int, input().split()))
+# ar.sort( )
+# print("min no la : " , max(ar))
+# print("max no la : " , min(ar))
+# for i in ar:
+#     if i % 2 == 0 :
+#         print( "chi so dau tien co gia tri chan : " , ar.index(i)+1)
+#         break
+
+# # print(ar.count(3))
+# if ar.count(3) > 0 :
+#     print("co so 3 cute")
+# else: print("k co so 3")
+
+      
+
+# k = int(input())
+# ar.insert(k,5)
+# print("mang co dang khi chen 5 vao vi tri k " , ar)
+
+# for i in ar:
+#     if i % 2 == 0:
+#         ar.remove(i)
+# print(ar)
+    
+
+# m = int(input())
+# b = list(map(int , input().split()))
+# b = b + b 
+# print(b)
+# b = b[::-1]
+# print(b) 
+# c = ar + b
+# print(c) 
+
+ 
